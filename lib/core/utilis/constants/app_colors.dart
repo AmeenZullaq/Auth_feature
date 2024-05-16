e681035b56d856f7c1fc5feab54b1525f0da8728
ameen <ameenemails@gmail.com>
2024-05-16 15:34:35 +0300

    add app styles

---
+++ lib/core/utilis/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const sugarWhite = Color(0xFFFAFFF8);
+  static const transparent = Color(0xFFA3C5B0);
+  static const greyText = Color(0xFFB6B6B6);
+  static const darkGreen = Color(0xFF035014);
+  static const darkButtonColor = Color(0xFF97BFA0);
+  static const lightButtoonColor = Color(0xFFB6E5B9);
+}
---
+++ lib/core/utilis/constants/app_styles.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:healthy_food_app/core/utilis/constants/app_colors.dart';
+
+abstract class AppStyles {
+  static TextStyle medium16Cabin = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cabin',
+    color: AppColors.transparent,
+  );
+  static TextStyle medium48Cabin = TextStyle(
+    fontSize: 48.sp,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cabin',
+  );
+  static TextStyle regular14Cabin = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Cabin',
+    color: AppColors.transparent,
+  );
+  static TextStyle medium14Cabin = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cabin',
+  );
+  static TextStyle medium18Cabin = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cabin',
+  );
+  static TextStyle medium12Cabin = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Cabin',
+  );
+  static TextStyle bold22Cabin = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Cabin',
+  );
+  static TextStyle regular16Mantaga = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montaga',
+    color: AppColors.darkGreen,
+  );
+  static TextStyle regular13Mantaga = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montaga',
+  );
+  static TextStyle regular12Mantaga = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montaga',
+  );
+  static TextStyle regular20Mantaga = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montaga',
+  );
+  static TextStyle regular22Mantaga = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montaga',
+  );
+}
